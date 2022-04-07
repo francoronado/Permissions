f370848b635175564454678b0dc296477a2b48dd
francoronado <45854853+francoronado@users.noreply.github.com>
2022-04-07 11:28:49 -0500

    Add files via upload

--- src/android/com/android/plugins/Permissions.java
+++ src/android/com/android/plugins/Permissions.java
@@ -141,7 +141,7 @@ public class Permissions extends CordovaPlugin {
         }
         
 
-	 else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+	 else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
     	if (permissions.getString(0).equals("android.permission.MANAGE_EXTERNAL_STORAGE")) {
         	Activity activity = this.cordova.getActivity();
         	Intent intent = new Intent();
